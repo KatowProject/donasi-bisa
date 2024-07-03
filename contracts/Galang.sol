cd35b607effa54f24debe1241535817873c27816
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-07-03 20:13:10 +0700

    first commit

---
+++ contracts/Galang.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract Galang {
+
+    address public owner;
+    mapping(uint256 => Penggalang) public GalangData;
+
+    /*
+    status = 0; onprogress
+    status = 1; withdrawn
+    status = 2; fraud
+    */
+    struct Penggalang {
+        address penggalang;
+        string nama;
+        string deskripsi;
+        uint256 target;
+        uint256 terkumpul;
+        uint256 deadline;
+        uint256 totalDonatur;
+        uint256 status;
+    }
+
+    mapping (uint256 => IDonatur[]) public donatur;
+
+    struct IDonatur {
+        address donatur;
+        uint256 value;
+    }
+
+    uint256 GalangDatalength = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+
+        _;
+    }
+
+    function setOwner(address _owner) public onlyOwner {
+        owner = _owner;
+    }
+
+    function createGalang(address _adr, string memory _nama, string memory _desc, uint256 _target, uint256 _deadline) public onlyOwner {
+        require(_adr != address(0), "Address cannot be 0x0");
+        require(_target > 0, "Target must be greater than 0");
+        require(_deadline > block.timestamp, "Deadline must be greater than current time");
+
+        GalangData[GalangDatalength] = Penggalang(_adr, _nama, _desc, _target, 0, _deadline, 0, 0);
+        GalangDatalength++;
+    }
+
+    function depo(uint256 _idGalang) public payable {
+        Penggalang memory galangData = GalangData[_idGalang];
+        require(galangData.penggalang != address(0), "Penggalang tidak di temukan");
+        require(msg.value > 0, "Tidak ada Value");
+        require(galangData.deadline > block.timestamp, "Galang dana sudah selesai");
+        uint256 toBeDeposit = msg.value;
+        if(toBeDeposit + galangData.terkumpul >= galangData.target) {
+            uint256 toRefund = toBeDeposit + galangData.terkumpul - galangData.target;
+            toBeDeposit = msg.value - toRefund;
+            payable(msg.sender).transfer(toRefund);
+        }
+
+        GalangData[_idGalang].terkumpul += toBeDeposit;
+        GalangData[_idGalang].totalDonatur++;
+        donatur[_idGalang].push(IDonatur(msg.sender, toBeDeposit));
+    }
+
+    function withdraw(uint256 _idGalang) public {
+        Penggalang memory galangData = GalangData[_idGalang];
+        require(galangData.penggalang != address(0), "Penggalang tidak di temukan");
+        require(galangData.penggalang == msg.sender, "Bukan penggalang dana");
+        require(galangData.deadline <= block.timestamp, "Penggalangan Dana belum selesai");
+        require(galangData.status == 0, "Sudah di Withdraw");
+        GalangData[_idGalang].status = 1;
+        payable(msg.sender).transfer(galangData.terkumpul);
+    }
+
+    function FraudDonation(uint256 _idGalang) public onlyOwner {
+        Penggalang memory galangData = GalangData[_idGalang];
+        require(galangData.penggalang != address(0), "Penggalang tidak di temukan");
+        require(galangData.status != 2, "Sudah ter refund");
+
+        for ( uint i =0; i < galangData.totalDonatur;i++) {
+            IDonatur memory Donatur = donatur[_idGalang][i];
+            payable(Donatur.donatur).transfer(Donatur.value); 
+        }
+
+        GalangData[_idGalang].status = 2;
+    }
+
+}
---
+++ test/Galang.ts
@@ -0,0 +1,66 @@
+import {
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+import BigNumber from "bignumber.js";
+
+async function sleep(ms: any) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+describe("Lock", function () {
+  async function deployOneYearLockFixture() {
+
+    const Galang = await hre.ethers.getContractFactory("Galang");
+    const galang = await Galang.deploy();
+
+    return { galang };
+  }
+
+  describe("Deployment", function () {
+    it("Should Create one Galang ID", async function () {
+      const { galang } = await loadFixture(deployOneYearLockFixture);
+      const me = await hre.ethers.getSigners();
+      this.address = me[0].address;
+
+      await galang.createGalang(
+        this.address,
+        "kotnol",
+        "kocok",
+        "1000000000000000000",
+        Math.floor((Date.now()) / 1000) + 6
+      )
+      this.galang = galang;
+      expect((await galang.GalangData(0)).penggalang).to.equal(
+        this.address);
+    });
+
+    it("Should deposit 0.5 eth success", async function () {
+      const toDeposit = new BigNumber("500000000000000000");
+      await this.galang.depo(0, { value: toDeposit.toString() });
+      expect((await this.galang.GalangData(0)).terkumpul).to.equal(toDeposit);
+
+    });
+
+    it("Should deposit 1 eth and refunded 0.5 success", async function () {
+      const toDeposit = new BigNumber("1000000000000000000");
+      await this.galang.depo(0, { value: toDeposit.toString() });
+      expect((await this.galang.GalangData(0)).terkumpul).to.equal(toDeposit);
+
+    });
+
+    it("Should revert not time", async function () {
+      await expect(this.galang.withdraw(0)).to.be.revertedWith("Penggalangan Dana belum selesai");
+
+    });
+
+    it("Should Success withdraw", async function () {
+      await sleep(5500);
+      await this.galang.withdraw(0)
+      
+      expect((await this.galang.GalangData(0)).status).to.equal(1);
+
+    });
+
+  });
+});
